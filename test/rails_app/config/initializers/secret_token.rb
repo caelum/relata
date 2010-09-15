7a3f2b9236d80ae44fac92e821a1f33a62b1dee4
Thiago Scalone <thiago@scalone.com.br>
2010-09-15 13:16:50 -0300

    Added RailsApp to tests

---
+++ test/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '9e8ac806fdf868fbf7d68a152c2213cb40c4ffcbc9c48744dc9459ad4fd314f2edb09ff0a4c7f121b3d57cfcdc4b0814166d21670af123eb6aaa57488235fd0a'
---
+++ test/rails_app/db/migrate/20100914142322_comment_add_column_post_id.rb
@@ -0,0 +1,9 @@
+class CommentAddColumnPostId < ActiveRecord::Migration
+  def self.up
+    add_column :comments, :post_id, :integer
+  end
+
+  def self.down
+    remove_column :comments, :post_id, :integer
+  end
+end
