1b9b910977b320ee29af3967a65156517fcbc1d2
Thiago Scalone <thiago@scalone.com.br>
2010-09-15 13:19:05 -0300

    Added require for init and rails_app in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
-require 'rubygems'
-require 'test/unit'
-require 'active_support'
+require File.expand_path(File.dirname(__FILE__)) + "/../init"
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__)) + "/rails_app/config/environment"
+require 'rails/test_help'
