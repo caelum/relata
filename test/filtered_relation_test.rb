9e5fb59b91174d297b73955e8875a667c5caec1e
Thiago Scalone <thiago@scalone.com.br>
2010-09-15 13:19:44 -0300

    Fix in filtered_relation

--- test/filtered_relation_test.rb
+++ test/filtered_relation_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__)) + "/test_helper"
 
 class FilteredRelationTest < ActiveSupport::TestCase
   setup do
@@ -10,12 +10,12 @@ class FilteredRelationTest < ActiveSupport::TestCase
   end
   
   test "given a content and comment filter, gives us filtered records" do 
-    @base.update_attribute(:content, "picture") 
+    @base.update_attributes(:content => "picture", :comments => [Comment.create]) 
     assert_equal @base, Post.filtered_relation(:content => "picture", :comments => true).first 
   end
   
   test "given a date and comment filter, gives us filtered records" do 
-    @base.update_attribute(:published_at, 2.years.ago) 
+    @base.update_attributes(:published_at => 2.years.ago, :comments => [Comment.create]) 
     assert_equal @base, Post.filtered_relation(:published_at => true, :comments => true).first 
   end 
 
@@ -39,6 +39,6 @@ class FilteredRelationTest < ActiveSupport::TestCase
     @base = Post.create(valid_attributes)
     @quote = Post.create(valid_attributes.merge(:content => "quote"))
     @number2 = Post.create(valid_attributes.merge(:user_id => 2))
-    @old = Post.create(valid_attribtues.merge(:published_at => 1.year.ago))
+    @old = Post.create(valid_attributes.merge(:published_at => 1.year.ago))
   end
 end
