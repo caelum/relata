6603afe04e78c42bd5467db27d296691b6e18b1e
Thiago Scalone <thiago@scalone.com.br>
2010-09-15 13:18:04 -0300

    Added bundler

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :gemcutter
+gem "rails", "3.0.0"
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'ruby-debug19', :require => "ruby-debug"
--- init.rb
+++ init.rb
@@ -1,1 +1,12 @@
+$:.unshift(File.join(File.dirname(__FILE__), "lib"))
+
+require "rubygems"
+require "bundler"
+Bundler.setup
+
+require 'rails'
+require 'ruby-debug'
+require 'active_record'
+require "active_support"
+
 require "filtered_relation"
