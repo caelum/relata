6b65324b139173d4f4f4022755b8c8bce49889be
anderson leite <andersonlfl@gmail.com>
2010-09-29 14:16:22 -0300

    beggining dsl test

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -1,19 +1,24 @@
-def custom
+require File.expand_path(File.dirname(__FILE__)) + "/test_helper"
+require 'filtered_relation/dsl'
 
-   posts = Post.filtered_relation.where(:comments).exists?
-   posts = posts.and(:authors).count.lt(3)
+class DSLTest < ActiveSupport::TestCase
+  def custom
+
+     posts = Post.filtered_relation.where(:comments).exists?
+     posts = posts.and(:authors).count.lt(3)
 
 
-   # Post.filtered_relation(:comments_count => >2)
-   Post.where("name.size > 2")
+     # Post.filtered_relation(:comments_count => >2)
+     Post.where("name.size > 2")
    
-   r = Post.where(:comments).count.exists?
-   r = Post.where(:comments).count.gt(2)
-   r = Post.where(:comments).count.gt(2)
-   r = Post.where(:name).size.gt(2)
-   r = Post.where(:name).like("%a%")
-   r = Post.where(:comments).name.like("%a%")
-   r = Post.where(:comments).name.gt(2)
-   # Author.where(:posts).comments.count.gt(2)
+     r = Post.where(:comments).count.exists?
+     r = Post.where(:comments).count.gt(2)
+     r = Post.where(:comments).count.gt(2)
+     r = Post.where(:name).size.gt(2)
+     r = Post.where(:name).like("%a%")
+     r = Post.where(:comments).name.like("%a%")
+     r = Post.where(:comments).name.gt(2)
+     # Author.where(:posts).comments.count.gt(2)
    
- end
+   end
+end
