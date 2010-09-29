087c7e26d80ce94fa051e62450b94502b8f68e13
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-09-29 15:32:41 -0300

    we all share our schemas

--- test/filtered_relation_test.rb
+++ test/filtered_relation_test.rb
@@ -2,38 +2,7 @@ require File.expand_path(File.dirname(__FILE__)) + "/test_helper"
 
 require 'active_record'
 require 'filtered_relation/filter'
-
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "test.sqlite3")
-
-def setup_db
-  
-    ActiveRecord::Base.connection.tables.each do |table|
-      ActiveRecord::Base.connection.drop_table(table)
-    end
-    
-  ActiveRecord::Schema.define(:version => 1) do
-
-     create_table :posts do |t|
-        t.string :body
-        t.string :title
-        t.text :content
-        t.integer :user_id
-        t.datetime :published_at
-        t.timestamps
-      end
-
-      create_table :users do |t|
-        t.timestamps
-      end
-      
-      create_table :comments do |t|
-        t.text :description
-        t.integer :post_id
-        t.timestamps
-      end
-      
-  end
-end
+require 'schema'
 
 class Post < ActiveRecord::Base
   belongs_to :user
---
+++ test/schema.rb
@@ -0,0 +1,31 @@
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "test.sqlite3")
+
+def setup_db
+  
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.drop_table(table)
+    end
+    
+  ActiveRecord::Schema.define(:version => 1) do
+
+     create_table :posts do |t|
+        t.string :body
+        t.string :title
+        t.text :content
+        t.integer :user_id
+        t.datetime :published_at
+        t.timestamps
+      end
+
+      create_table :users do |t|
+        t.timestamps
+      end
+      
+      create_table :comments do |t|
+        t.text :description
+        t.integer :post_id
+        t.timestamps
+      end
+      
+  end
+end
