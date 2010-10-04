0a5ed9d8dde1b3327b173116d3e399faa580eda5
anderson leite <andersonlfl@gmail.com>
2010-10-04 11:42:47 -0300

    merger tests

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -95,6 +95,14 @@ class DSLTest < ActiveSupport::TestCase
     assert_equal 1, posts.size
   end
 
+  test "all post which commits has some subject" do
+    comment = Comment.create :subject => "dsl subject"
+    @caelum.update_attributes :comments => [comment]
+    posts = Post.where(:comments).subject.like?("%dsl subject%")
+    assert_equal @caelum, posts[0]
+    assert_equal 1, posts.size
+  end
+  
   # test "exists posts aaaaaaaaaaaaa" do
   #   @caelum.update_attributes :published_at => 1.year.ago
   #   
@@ -112,13 +120,5 @@ class DSLTest < ActiveSupport::TestCase
     # posts = posts.and(:authors).count.lt(3)
     # Author.where(:posts).comments.count.gt(2)
    # end
-
-  test "all post which commits has some subject" do
-    comment = Comment.create :subject => "dsl subject"
-    @base.update_attributes :comments => [comment]
-    posts = Post.where(:comments).subject.like?("%dsl subject%")
-    assert_equal @base, posts[0]
-    assert_equal 1, posts.size
-  end
 
 end
