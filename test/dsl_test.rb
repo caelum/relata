76fcc25e312f24e6e97d7293461fb51f477acce6
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-05 10:54:28 -0300

    supports AND in dsl

--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -118,16 +118,17 @@ class DSLTest < ActiveSupport::TestCase
     assert_equal 1, posts.size
   end
   
-  # test "exists posts aaaaaaaaaaaaa" do
-  #   @caelum.update_attributes :published_at => 1.year.ago
-  #   
-  #   posts = Post.where { 
-  #     published_at.between(2.years.ago, 6.months.ago)
-  #     name.like?
-  #      }
-  #   assert_equal @caelum, posts[0]
-  #   assert_equal 1, posts.size
-  # end
+  test "accepts two conditions" do
+    @caelum.update_attributes :published_at => 1.year.ago
+    @guilherme.update_attributes :published_at => 1.year.ago
+    
+    posts = Post.where { 
+      published_at.between(2.years.ago, 6.months.ago)
+      body.like?("%lum%")
+    }
+    assert_equal @caelum, posts[0]
+    assert_equal 1, posts.size
+  end
 
   # def pending
     # posts = Post.where { comments.description.like?("%dsl test%") }
